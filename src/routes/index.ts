2bef610c6aa70d6c6134ae4ecfa609fff241ac45
eliseu <eliseudaniel29@gmail.com>
2025-08-07 09:40:00 -0300

    init clients

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {Teste, createUser, getAll, getUser} from '../controllers/UserController'
+import { findAll } from '../controllers/ClientsController';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get('/users/:id', getUser);
 router.post('/users', createUser);
 
 
+router.get('/clients', findAll);
 
 export default router;
